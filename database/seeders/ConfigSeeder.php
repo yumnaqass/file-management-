1eb78012992df5b2720c668526eb55fa49f0089b
yumnaqass <yumnaqassomah@email.com>
2023-12-15 19:42:30 +0200

    15/12

---
+++ database/seeders/ConfigSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ConfigSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('configs')->insert([
+            'file_number' => 50
+        ]);
+    }
+}
